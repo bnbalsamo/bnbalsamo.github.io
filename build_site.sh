7e26182934fed3d0b4b8f2ec677a1f89a7369d99
brian <balsamo@uchicago.edu>
2018-03-05 14:57:52 -0600

    It probably helps to install gems/do builds in the right directories

--- build_site.sh
+++ build_site.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -v $(pwd):/srv/jekyll jekyll/jekyll sh -c 'gem install minima && jekyll build'
+docker run --rm -v $(pwd):/srv/jekyll jekyll/jekyll sh -c 'cd /srv/jekyll && gem install minima && jekyll build'
